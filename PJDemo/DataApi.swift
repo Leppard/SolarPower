c5acccc6ab2a360b69dc0ba0f74c22388ae7f013
Leppard <coldlefthand@gmail.com>
2016-04-29 13:16:37 +0800

    import Alamofire

--- PJDemo/DataApi.swift
+++ PJDemo/DataApi.swift
@@ -7,13 +7,19 @@
 //
 
 import Foundation
+import Alamofire
 
 let urlSchema = "http://15013vr175.imwork.net:7777"
 
 class DataApi {
     static func getDayData(success: (NSDictionary) -> Void) {
         let urlString = urlSchema.stringByAppendingString(kConsume_Day_Data)
-        
-        
+        Alamofire.request(.GET, urlString, parameters: nil)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+        }
     }
 }
